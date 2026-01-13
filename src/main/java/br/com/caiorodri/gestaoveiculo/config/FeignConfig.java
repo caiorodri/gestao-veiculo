9c08ac6484970a09d19a17ad214484966e90b5ea
caiorodri <caiodossantosrodrigues@yahoo.com.br>
2026-01-13 18:48:49 -0300

    chore(config): adicionar configurações de CORS e Feign Client

---
+++ src/main/java/br/com/caiorodri/gestaoveiculo/config/FeignConfig.java
@@ -0,0 +1,16 @@
+package br.com.caiorodri.gestaoveiculo.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            requestTemplate.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+        };
+    }
+}
