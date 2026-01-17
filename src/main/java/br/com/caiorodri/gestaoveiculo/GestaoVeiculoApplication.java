d795bd58aad42b28ed49f7b82820a36ee1ca8a05
caiorodri <caiodossantosrodrigues@yahoo.com.br>
2026-01-17 19:18:51 -0300

    chore(application): adicionar anotação necessária para utilizar @Async

--- src/main/java/br/com/caiorodri/gestaoveiculo/GestaoVeiculoApplication.java
+++ src/main/java/br/com/caiorodri/gestaoveiculo/GestaoVeiculoApplication.java
@@ -3,11 +3,13 @@ package br.com.caiorodri.gestaoveiculo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EnableAsync
 public class GestaoVeiculoApplication {
 
 	public static void main(String[] args) {
